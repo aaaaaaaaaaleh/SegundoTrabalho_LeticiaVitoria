911b38d39380ad7ff7704ac3dc41daf59bdc86e1
LeticiaCastelo <leticia.castelo@aluno.ifsp.edu.br>
2022-05-13 09:09:37 -0300

    Initial commit

---
+++ app/src/main/java/leticia/segundotrabalho_leticiavitoria/MainActivity.java
@@ -0,0 +1,16 @@
+package leticia.segundotrabalho_leticiavitoria;
+
+// Letícia Castelo e Vitória Hilgert
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
